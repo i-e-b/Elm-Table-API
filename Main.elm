a847d94d82446e0ed2e05456cf36960062895185
Iain Ballard <iain.ballard@7digital.com>
2013-11-14 16:21:46 +0000

    rows, needs cleaning

--- Main.elm
+++ Main.elm
@@ -5,7 +5,8 @@ divisions percents size =
   let x s = map (\f -> round(f * (toFloat s))) percents
   in x size
   
-
+-- Given a list of elements, and a list of column widths, 
+-- output a list of sized elements
 makeRow : [Element] -> [Int] -> [Element]
 makeRow elements widths =
   let pairs = zip elements widths
@@ -13,11 +14,11 @@ makeRow elements widths =
 
 
 exampleElements = map (plainText) ["One", "Two", "Three", "Four"]
-exampleWidths = [0.5, 0.1, 0.2, 0.3]
+exampleWidths = divisions [0.5, 0.1, 0.2, 0.3] <~ Window.width
 
-exampleRow = 
-  flow right
-  <~ ((makeRow exampleElements) <~ (divisions exampleWidths <~ Window.width))
+exampleRow =
+  let sizedElements = (makeRow exampleElements) <~ (exampleWidths)
+  in flow right <~ sizedElements
 
 
-main = exampleRow
+main = flow down <~ (combine [exampleRow, exampleRow, exampleRow, exampleRow])
